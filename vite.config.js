55ffd8b850295f9f8de594fee2aac0eab5c5de62
Chubbyduner <1262426172@qq.com>
2025-07-07 10:54:07 +0800

    chore(deps): update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,52 @@
-import { defineConfig } from 'vite'
+import { loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from "node:url";
+import { root, alias, pathResolve, warpperEnv } from "./build/utils";
+
 
 // https://vite.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default ({ mode }) => {
+  const { VITE_PORT, VITE_PUBLIC_PATH } = warpperEnv(loadEnv(mode, root));
+  return {
+    base: VITE_PUBLIC_PATH,
+    root,
+    resolve: {
+      alias
     },
-  },
-  plugins: [vue()],
-})
+    // 服务端渲染
+    server: {
+      // 端口号
+      port: VITE_PORT,
+      host: "0.0.0.0",
+      // 本地跨域代理 https://cn.vitejs.dev/config/server-options.html#server-proxy
+      proxy: {},
+      // 预热文件以提前转换和缓存结果，降低启动期间的初始页面加载时长并防止转换瀑布
+      warmup: {
+        clientFiles: ["./index.html", "./src/{views,components}/*"]
+      }
+    },
+    plugins: [vue()],
+    build: {
+      // https://cn.vitejs.dev/guide/build.html#browser-compatibility
+      target: "es2015",
+      sourcemap: false,
+      // 消除打包大小超过500kb警告
+      chunkSizeWarningLimit: 4000,
+      rollupOptions: {
+        input: {
+          index: pathResolve("./index.html", import.meta.url)
+        },
+        // 静态资源分类打包
+        output: {
+          chunkFileNames: "static/js/[name]-[hash].js",
+          entryFileNames: "static/js/[name]-[hash].js",
+          assetFileNames: "static/[ext]/[name]-[hash].[ext]",
+          // 手动分包
+          manualChunks: {
+            vue: ["vue", "Pinia", "vue-router"],
+            "ant-design-vue": ["ant-design-vue"],
+          }
+        }
+      }
+    }
+  };
+}
