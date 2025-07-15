c425fd557ca39e1727769132be363b0211f144c5
Chubbyduner <1262426172@qq.com>
2025-07-15 18:22:30 +0800

    feat: 更新网站Logo和名称

--- src/components/Lottery/pages/LotteryLogo.vue
+++ src/components/Lottery/pages/LotteryLogo.vue
@@ -4,7 +4,7 @@
   <div class="lottery-logo">
     <img src="@/assets/images/yun.png" class="cloud-left" alt="云朵" />
     <div class="logo-text">
-      <h1>抽奖系统</h1>
+      <h1>乐抽GO</h1>
       <p>LOTTERY SYSTEM</p>
     </div>
     <img src="@/assets/images/yun.png" class="cloud-right" alt="云朵" />
